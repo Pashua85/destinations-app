d75578bfd3cef79985a015f8e034f258c72aed91
Pashua85 <pavellashuk85@gmail.com>
2018-04-16 20:50:12 +0500

    remove dumb data

--- src/components/App.js
+++ src/components/App.js
@@ -24,40 +24,7 @@ function geoCodeLocation(latLng) {
 
 class App extends React.Component {
   state = {
-    addresses: [
-      // {
-      //   address: 'ул. Щорса, 94А, Екатеринбург, Свердловская обл., Россия, 620144',
-      //   latLng: {
-      //     lat: 56.80950300000001,
-      //     lng: 60.606506999999965
-      //   },
-      //   placeId: "ChIJc8sXNttuwUMRWSTSqEBCxrY"
-      // },
-      // {
-      //   address: 'ул. Чайковского, 62, Екатеринбург, Свердловская обл., Россия, 620130',
-      //   latLng: {
-      //     lat: 56.8073729,
-      //     lng: 60.62031009999998
-      //   },
-      //   placeId: 'ChIJASguT89uwUMRMbx0H8V2uNk'
-      // },
-      // {
-      //   address: 'ул. Студенческая, 3, Екатеринбург, Свердловская обл., Россия, 620137',
-      //   latLng: {
-      //     lat: 56.854948,
-      //     lng: 60.661017000000015
-      //   },
-      //   placeId: 'ChIJ6TiVJjVswUMRRi8Evo28BM4'
-      // },
-      // {
-      //   address: 'Москва, Россия',
-      //   latLng: {
-      //     lat: 55.755826,
-      //     lng: 37.617299900000035
-      //   },
-      //   placeId: '"ChIJybDUc_xKtUYRTM9XV8zWRD0"'
-      // }
-    ],
+    addresses: [],
     errorMessage: '',
     pathCoords: []
   };
